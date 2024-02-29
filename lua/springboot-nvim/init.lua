b8d186fc9548a30f7d07d0d445b2073cd50b55b0
elmcgill <ethanlmcgill@gmail.com>
2024-02-28 21:30:56 -0600

    trying to determine if we are in a spring boot project and return the root directory

--- lua/springboot-nvim/init.lua
+++ lua/springboot-nvim/init.lua
@@ -1,9 +1,22 @@
-print("Hello vim pluglin")
+local lspconfig = require("lspconfig")
 
-local function some_function()
-    print("hello from function")
+local function get_spring_boot_project_root()
+    local current_file = vim.fn.expand("%:p")
+    local root_pattern = {"pom.xml", "build.gradle", ".git" }
+
+    return lspconfig.util.root_pattern(table.unpack(root_pattern))(current_file)
+end
+
+local function boot_run()
+    local project_root = get_spring_boot_project_root()
+
+    if project_root then
+        print("Spring Boot project root: ", project_root)
+    else
+        print("Not in a Spring Boot project")
+    end
 end
 
 return {
-    some_function = some_function
+    boot_run = boot_run
 }
